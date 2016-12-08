d413d9d9201430d99a80bf51b91e28a36bebdf90
Cait Friedlander <caitfinchart@gmail.com>
2016-12-08 15:01:16 -0800

    add history

--- js/app.js
+++ js/app.js
@@ -10,44 +10,14 @@ function Square(props) {
 // Header with game information above 3x3 grid
 class Board extends React.Component {
 
-  constructor() {
-    super();
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true
-    }
-  }
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? "X" : "O";
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext});
-  }
 
   renderSquare(i) {
-
-    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)}/>;
-
+    return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)}/>;
   }
+
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = "Winner: " + winner;
-    } else {
-      status = "Next Player: " + (this.state.xIsNext ? "X" : "O");
-    }
-    // Notice how the Squares are being included. Unlike Angular, React
-    // uses single braces to bind data. This is an early sight of the
-    // one-way data binding React is known for.
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -70,14 +40,51 @@ class Board extends React.Component {
 
 // Gathers everything together for additional styling
 class Game extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null)
+      }],
+      xIsNext: true
+    };
+  }
+
+  handleClick(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      history: history.concat([{
+        squares: squares
+      }]),
+      xIsNext: !this.state.xIsNext
+    })
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+
+    let status;
+    if (winner) {
+      status= "Winner: " + winner;
+    } else {
+      status = "Next Player: " + (this.state.xIsNext ? "X" : "O");
+    }
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board squares={current.squares} onClick={(i) => this.handleClick(i)} />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
