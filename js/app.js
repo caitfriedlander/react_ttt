b7ed00d44f5641e473942d20a6e53d0a9f2801c6
Cait Friedlander <caitfinchart@gmail.com>
2016-12-08 15:18:43 -0800

    add moves

--- js/app.js
+++ js/app.js
@@ -71,6 +71,14 @@ class Game extends React.Component {
     const history = this.state.history;
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
+    const moves = history.map((step, move) => {
+      const desc = move ? "Move #" + move : "Game Start";
+        return (
+          <li key={move}>
+            <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
+          </li>
+        )
+    })
 
     let status;
     if (winner) {
@@ -85,7 +93,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
